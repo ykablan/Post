67b49c5b49b39169d1b1c19e22de5786d12aec9c
ykablan <ahmet.norpa@gmail.com>
2025-02-15 14:05:47 +0300

    blog_s1_v1

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -13,4 +13,5 @@ class PostAdmin(admin.ModelAdmin):
         model = Post
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
 
